742cdda06c85876e2f02159b4abdea8e8b2b012a
Enky <enkh6at@yahoo.com>
2026-04-22 18:39:16 +0100

    login logout

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -18,15 +18,69 @@ const generateToken = (user) => {
   });
 };
 
+// Register route
+
 router.post("/register", async (req, res) => {
   const { username, password, email } = req.body;
   if (!username || !password || !email) {
     return res.status(400).json({ error: "Username, password and email are required" });
   }
-  const userExists = await pool.query("SELECT * FROM users WHERE username = $1", [username]);
+  const userExists = await pool.query("SELECT * FROM users WHERE email = $1", [email]);
   if (userExists.rows.length > 0) {
-    return res.status(400).json({ error: "Username already exists" });
+    return res.status(400).json({ message: "Email already exists" });
   };
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const newUser = await pool.query(
+      "INSERT INTO users (username, email, password) VALUES ($1, $2, $3) RETURNING *",
+      [username, email, hashedPassword]
+    );
+
+    const token = generateToken(newUser.rows[0].id);
+    res.cookie("token", token, cookiesOptions);
+    res.status(201).json({ message: "User registered successfully", user: newUser.rows[0] });
 });
 
+// Login route
+
+router.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+  if (!email || !password) {
+    return res.status(400).json({ error: "Email and password are required" });
+  }
+    const user = await pool.query("SELECT * FROM users WHERE email = $1", [email]); 
+
+    if (user.rows.length === 0) {
+      return res.status(400).json({ message: "Invalid email or password" });
+    };
+
+    const userData = user.rows[0];
+
+    const isMatch = await bcrypt.compare(password, userData.password);
+
+    if (!isMatch) {
+      return res.status(400).json({ message: "Invalid email or password" });
+    };
+
+    const token = generateToken(userData.id);
+    res.cookie("token", token, cookiesOptions);
+    res.json({ message: "Logged in successfully", user: userData });
+
+
+})
+
+// me route
+
+router.get("/me", async (req, res) => {
+    res.json({ user: req.user });
+    // return info of the logged in user from protect middleware
+})
+
+// Logout route
+
+router.post("/logout", (req, res) =>
+{  res.clearCookie("token", cookiesOptions);
+  res.json({ message: "Logged out successfully" });
+});
 export default router;   
