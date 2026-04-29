58d9c3c5665c5ffedacb2f96f97d3d20be8d955d
Enky <enkh6at@yahoo.com>
2026-04-29 13:40:53 +0100

    app.jsx completing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,38 @@
-import { useState } from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
-
+import { use, useEffect, useState } from 'react';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import axios from 'axios';
 
 function App() {
   const [user, setUser] = useState(null);
   const [error, setError] = useState("");
   const [loading, setLoading] = useState(true);
 
+  axios.defaults.withCredentials = true;
+
+  useEffect(() => {
+    const fetchUser = async () => {
+      try {
+        const res = await axios.get('http://localhost:5000/api/auth/me');
+        setUser(res.data);
+      } catch (err) {
+        setError(err.response.data.message);
+      } finally {
+        setLoading(false);      
+    }
+  };
+    fetchUser();
+  }, []);
+
+  if (loading) return <div>Loading...</div>;
   return (
     <Router>
-
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<h1>Home</h1>} />
+        <Route path="/login" element={<h1>Login</h1>} />
+        <Route path="/register" element={<h1>Register</h1>} />
+      </Routes>
     </Router>
   );
 }
